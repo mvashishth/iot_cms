1acc39b276e4e12138599cbb827576903083b6f0
Mayur <vashishthmayur@gmail.com>
2019-04-13 18:05:57 +0530

    Added requirements.txt

--- hello.py
+++ hello.py
@@ -2,10 +2,23 @@ from flask import Flask, Response, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET','POST'])
-def get_data():
-    print("HELLO")
-    return Response(request.data)
+@app.route("/")
 
+def hello():
+	return "Hello World!"
+
+@app.route("/index")
+def index():
+	user = {'username': 'Miguel'}
+	return '''
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
 if __name__ == '__main__':
     app.run(debug=True)
+
